cb9e1700e63c97fbbfe514c9c2a95b14f143dfd8
Chilas Main PC <Chil0041@edu.easj.dk>
2021-10-28 13:13:31 +0200

    ..

--- controllers/authorController.js
+++ controllers/authorController.js
@@ -5,10 +5,19 @@ exports.author_list = function(req, res) {
     res.send('NOT IMPLEMENTED: Author list');
 };
 
-// Display detail page for a specific Author.
-exports.author_detail = function(req, res) {
-    res.send('NOT IMPLEMENTED: Author detail: ' + req.params.id);
-};
+// Display list of all Authors.
+// Display list of all Authors.
+exports.author_list = function(req, res, next) {
+
+    Author.find()
+      .sort([['family_name', 'ascending']])
+      .exec(function (err, list_authors) {
+        if (err) { return next(err); }
+        //Successful, so render
+        res.render('author_list', { title: 'Author List', author_list: list_authors });
+      });
+  
+  };
 
 // Display Author create form on GET.
 exports.author_create_get = function(req, res) {
